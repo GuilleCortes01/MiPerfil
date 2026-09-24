28fe6606c828a56a4962711a603db22fed1272ea
GuilleCortes01 <guillecortes100@gmail.com>
2026-09-24 01:20:57 -0300

    fix: importa Misiones en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Inventario from './components/Inventario'
+import Misiones from './components/Misiones'
 import PantallaTitulo from './components/PantallaTitulo'
 import TarjetaOperador from './components/TarjetaOperador'
 
